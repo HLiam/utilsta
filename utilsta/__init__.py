d07b46787f2e64ffdf1085b4382c9e764bcbe474
Liam Henrickson <liam.henrickson@gmail.com>
2020-05-25 14:27:42 -0600

    Import modules with package

--- utilsta/__init__.py
+++ utilsta/__init__.py
@@ -2,3 +2,10 @@
 
 __version__ = '0.1.0'
 __author__ = 'Liam Henrickson'
+
+import utilsta.altimeter
+import utilsta.controller
+import utilsta.device
+import utilsta.haptic
+from utilsta.exceptions import AlreadyRunningError, NoControllerConnectedError
+# import utilsta.memory
--- utilsta/exceptions.py
+++ utilsta/exceptions.py
@@ -4,4 +4,3 @@ class AlreadyRunningError(Exception):
 
 class NoControllerConnectedError(Exception):
     pass
-
